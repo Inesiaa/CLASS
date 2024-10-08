45839c8d0fff44f8df57ba23a4361d47077303f3
Inesiaa <brujinesita@gmail.com>
2024-10-08 09:59:16 +0200

    color_change

---
+++ Assets/Scripts/scriptDePrueba.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class scriptDePrueba : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.grey;
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            GetComponent<Renderer>().material.color = Color.black;
+        }
+    }
+}
