92856715a0e9e0ab41a1b720141e1a318f155e17
Inesiaa <brujinesita@gmail.com>
2024-10-09 10:56:52 +0200

    trigger_and_color

--- Assets/Scripts/scriptDePrueba.cs
+++ Assets/Scripts/scriptDePrueba.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class scriptDePrueba : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
     void Start()
     {
         
@@ -10,6 +12,18 @@ public class scriptDePrueba : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        
+        
+        translation *= Time.deltaTime; // Make it move 10 meters per second instead of 10 meters per frame...
+        rotation *= Time.deltaTime;
+        
+        transform.Translate(0, 0, translation); // Move translation along the object's z-axis
+        
+        transform.Rotate(0, rotation, 0); //Rotate around our y-axis
+
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             GetComponent<Renderer>().material.color = Color.red;
